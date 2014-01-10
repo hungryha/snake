0998c3f1df762ec13a0e7f14e17f7f2bc46fe300
Qian Long <qlong@mit.edu>
2014-01-10 16:48:10 -0500

    Refactor

--- reference/snake.js
+++ reference/snake.js
@@ -5,71 +5,101 @@ window.onload = function() {
   var ctx = canvas.getContext("2d");
   var boardwidth = canvas.width;
   var boardheight = canvas.height;
-  console.log("width: " + boardwidth);
-  console.log("height: " + boardheight);
 
   // Game variables
-  var cell_width = 10; // partitions board into cells of length 10px
+  var cellwidth = 10; // partitions board into cells of length 10px
   var direction;
   var food;
   var score;
-  var snake_array; // array of cells that make up the snake
+  var snake_array; // array of cells that make up the snake, grows each time food is eaten
 
-
-  function create_snake() {
+  function initSnake() {
     var length = 5;
     snake_array = [];
     for (var i = length-1; i >= 0; i--) {
       var cell = {x:i, y:0};
       snake_array.push(cell);
     }
-
   }
 
-
-  function create_food() {
+  function createFood() {
     // Randomly places food on the board
-    var xpt = Math.round(Math.random()*(boardwidth - cell_width) / cell_width);
-    var ypt = Math.round(Math.random()*(boardheight - cell_width) / cell_width);
+    var xpt = Math.round(Math.random()*(boardwidth - cellwidth) / cellwidth);
+    var ypt = Math.round(Math.random()*(boardheight - cellwidth) / cellwidth);
 
     // x and y between 0 and 44 inclusive
     food = {x:xpt, y:ypt};
   }
 
-  //create_food();
-  //console.log(food);
+  function paintCell(x, y) {
+    // Helper function to paint one cell
+    ctx.fillStyle = "white";
+    ctx.fillRect(x*cellwidth, y*cellwidth, cellwidth, cellwidth);
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(x*cellwidth, y*cellwidth, cellwidth, cellwidth);
+  }
 
-  function paint() {
+  function selfCollision(x, y, array) {
+    // Array is array of {x, y} objects
+    // Returns true if cell (x,y) is part of array cells
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].x == x && array[i].y == y) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-    // Paints canvas background white with black border
+  // Keypress event listener
+  document.onkeydown = function(e){
+    var key = e.which;
+
+    // Second clause to prevents reverse gear
+    if(key == "37" && direction != "right") direction = "left";
+    else if(key == "38" && direction != "down") direction = "up";
+    else if(key == "39" && direction != "left") direction = "right";
+    else if(key == "40" && direction != "up") direction = "down";
+  }
+
+  function paintCanvas() {
+
+    // Paint canvas background
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, boardwidth, boardheight);
     ctx.strokeStyle = "black";
-    ctx.strokeRect(0, 0, boardwidth, boardheight); 
+    ctx.strokeRect(0, 0, boardwidth, boardheight);
 
+    // Paint Snake
+    for (var i = 0; i < snake_array.length; i++) {
+      var cell = snake_array[i];
+      paintCell(cell.x, cell.y);
+    }
+
+    // Paint Food
+    paintCell(food.x, food.y);
+
+    // Paint score
+    var score_text = "Score: " + score;
+    ctx.fillText(score_text, 5, boardheight-5);
+
+  }
+
+  function updateSnake() {
     var head_x = snake_array[0].x;
     var head_y = snake_array[0].y;
-   
-    // Assigns new head position based on direction
-    if (direction == "right") {
-      head_x++;
-    } 
-    else if (direction == "left") {
-      head_x--;
-    }
-    else if (direction == "up") {
-      head_y--;
-    }
-    else if (direction == "down") {
-      head_y++;
-    }
 
-    // Check for wall collisions
-    if (head_x >= boardwidth/cell_width || 
-        head_x <= -1 || 
-        head_y >= boardheight/cell_width ||
+    // Assigns new head position based on direction
+    if (direction == "right") head_x++;
+    else if (direction == "left") head_x--;
+    else if (direction == "up") head_y--;
+    else if (direction == "down") head_y++;
+
+    // Check for wall collisions and self collision
+    if (head_x >= boardwidth/cellwidth ||
+        head_x <= -1 ||
+        head_y >= boardheight/cellwidth ||
         head_y <= -1 ||
-        self_collision(head_x, head_y, snake_array)) {
+        selfCollision(head_x, head_y, snake_array)) {
 
       // gameover, restart
       init();
@@ -81,80 +111,39 @@ window.onload = function() {
       score++;
       // Creates new head, so snake becomes longer
       var tail = {x:head_x, y:head_y};
-      create_food();
+      createFood();
     }
     else {
       var tail = snake_array.pop();
       tail.x = head_x;
       tail.y = head_y;
     }
+
     // Moves snake
-    snake_array.unshift(tail) // Appends to beginning of array
-
-    // Paint snake
-    for (var i = 0; i < snake_array.length; i++) {
-      var cell = snake_array[i];
-      paint_cell(cell.x, cell.y);
-    }
-
-    // Paint food
-    paint_cell(food.x, food.y);
-
-    // Paint score
-    var score_text = "Score: " + score;
-    ctx.fillText(score_text, 5, boardheight-5);
+    snake_array.unshift(tail) // Appends tail to beginning of array
   }
 
-  function paint_cell(x, y) {
-    // Colors a cell  
-    ctx.fillStyle = "white";
-    ctx.fillRect(x*cell_width, y*cell_width, cell_width, cell_width);
-    ctx.strokeStyle = "white";
-    ctx.strokeRect(x*cell_width, y*cell_width, cell_width, cell_width);
-  }
+  function render() {
+    // paint board, snake, and food
+    paintCanvas();
 
-  function self_collision(x, y, array) {
-    // array is array of {x, y} objects
-    // Returns true if cell (x,y) is part of array cells
-    for (var i = 0; i < array.length; i++) {
-      if (array[i].x == x && array[i].y == y) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  // Event listeners
-  document.onkeydown = function(e){
-    var key = e.which;
-    // Second clause to prevents reverse gear
-    if(key == "37" && direction != "right") {
-      direction = "left";
-    }
-    else if(key == "38" && direction != "down") {
-      direction = "up";
-    }
-    else if(key == "39" && direction != "left") {
-      direction = "right";
-    }
-    else if(key == "40" && direction != "up") {
-      direction = "down";
-    }
+    // update snake position, detects wall and food collisions
+    updateSnake();
   }
 
   function init()
   {
-    direction = "right"; //default direction
-    create_snake();
-    create_food(); //Now we can see the food particle
-    //finally lets display the score
+    // Set default direction to right
+    direction = "right";
+    initSnake();
+    createFood();
     score = 0;
-    
-    //Lets move the snake now using a timer which will trigger the paint function
-    //every 60ms
-    if(typeof game_loop != "undefined") clearInterval(game_loop);
-    game_loop = setInterval(paint, 60);
+
+    if(typeof game_loop != "undefined") {
+      clearInterval(game_loop);
+    }
+    game_loop = setInterval(render, 60);
   }
 
-  init(); 
+  init();
 }
